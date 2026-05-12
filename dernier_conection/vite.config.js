3c49045bce9c32889644debb48f6d08d5395b36c
Dernier <dernier.bruno.teixeira00@aluno.ifce.edu.br>
2026-05-12 14:51:18 -0300

    chore: use root base in dev, keep GH Pages base in prod

--- dernier_conection/vite.config.js
+++ dernier_conection/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/dernier_portifolio/',
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const isDev = mode === 'development' || command === 'serve'
+  return {
+    base: isDev ? '/' : '/dernier_portifolio/',
+    plugins: [react()],
+  }
 })
